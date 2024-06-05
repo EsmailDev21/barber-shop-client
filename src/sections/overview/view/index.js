2c8c27413648697383c05479dc20fff3984a0016
esmail <smail.anonymous8@gmail.com>
2024-06-06 00:41:02 +0200

    fix

--- src/sections/overview/view/index.js
+++ src/sections/overview/view/index.js
@@ -1,2 +1,2 @@
 export { default as AdminAnalyticsView } from './app-view';
-export { default as BarberAnalyticsView } from './barbar-analytics-view';
+export { BarberAnalyticsView } from './barbar-analytics-view';
